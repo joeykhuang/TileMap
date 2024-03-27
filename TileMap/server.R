f58293c8910b1005f69b6dab1c4becf8f0e4bd75
joeykhuang <joeykhuang@gmail.com>
2024-03-26 21:36:27 -0400

    added dataset files and updated server-side UI

--- TileMap/server.R
+++ TileMap/server.R
@@ -7,6 +7,14 @@ library(forcats)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
+    
+    fileName <- reactive({input$dataset})
+    res <- reactive({
+      if(!is.null(fileName)){
+        read.csv(paste0("datasets/", fileName(), ".csv"))
+      }})
+    
+    
     fold_cat_colors <- c("-2" = "#103D80",
                          "-1.5" = "#3A7EBD",
                          "-0.75" = "#6FA5CC",
@@ -16,48 +24,47 @@ function(input, output, session) {
                          "1.5" = "#E16D54",
                          "2" = "#AA1B1B")
     
-    fc_sample_pivot$fold_cat <- factor(fc_sample_pivot$fold_cat, levels = c("-2", "-1.5", "-0.75", "-0.25", "2", "1.5", "0.75", "0.25"))
-    fc_sample_pivot$highlight <- as.factor(ifelse(fc_sample_pivot$gene == "GUF1", 1, 0.9))
-    print(fc_sample_pivot$highlight)
-    fc_sample_pivot <- fc_sample_pivot[order(abs(fc_sample_pivot$fold),decreasing=F),]
-    #fc_sample_pivot_plot <- fc_sample_pivot[fc_sample_pivot$condition %in% selected_conditions, ]
-    #fc_sample_pivot_plot <- fc_sample_pivot
-    
-    fc_sample_pivot_plot <- highlight_key(fc_sample_pivot, ~gene)
+    fc_sample_pivot <- reactive({pivot_longer(res(), cols = !X, names_to = "gene", values_to = "fold") %>%
+        rename(condition = X) %>%
+        mutate(val = as.integer(fold > 0) * 2 - 1) %>%
+        mutate(fold_cat = case_when(
+          fold < -2 ~ -2,
+          fold <= -1 ~ -1.5,
+          fold <= -0.25 ~ -0.75,
+          fold <= 0 ~ -0.25,
+          fold <= 0.25 ~ 0.25,
+          fold <= 1 ~ 0.75,
+          fold <= 2 ~ 1.5,
+          fold > 2 ~ 2,
+        )) %>%
+        mutate(fold_cat = factor(fold_cat,  levels = c("-2", "-1.5", "-0.75", "-0.25", "2", "1.5", "0.75", "0.25")))})
     
     whichplot <- reactiveVal(FALSE)
     observeEvent(input$button, {
       whichplot(!whichplot())
     })
     
-    conditionPlot <- highlight(ggplotly(ggplot(data = highlight_key(fc_sample_pivot, ~gene), 
-                        aes(y=fct_reorder(condition, val), x=val, fill=fold_cat, text=gene)) +
-                          geom_col(position="stack",alpha = 1, linewidth=0.2, color='black') + 
-                          geom_vline(xintercept=0, size=1) +
-                          theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank()) +
-                          scale_fill_manual(values=fold_cat_colors, name="Fold Change", 
-                                            breaks=c("-2", "-1.5", "-0.75", "-0.25", "0.25", "0.75", "1.5", "2")), tooltip = "text"), 
-                        on = "plotly_hover", opacityDim=0.3)
-    
-    genePlot <- highlight(ggplotly(ggplot(data = highlight_key(fc_sample_pivot, ~condition), 
-                                aes(x=fct_reorder(gene, val), y=val, fill=fold_cat, text=condition)) +
-                           geom_col(position="stack",alpha=1,color='black',linewidth=0.2) + 
-                           geom_hline(yintercept=0, size=1) +
-                           theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank(),
-                                 axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-                           scale_fill_manual(values=fold_cat_colors, name="Fold Change", 
-                                             breaks=c("-2", "-1.5", "-0.75", "-0.25", "0.25", "0.75", "1.5", "2")), tooltip = "text"),
-                          on = "plotly_hover", opacityDim=0.3)
-    
-    which_graph <- reactive({
-      if (whichplot()) {
-        genePlot
-      } else {
-        conditionPlot
-      }
-    })
-    
     output$tilePlot <- renderPlotly({   
-      which_graph()
+      if (whichplot()) {
+        highlight(ggplotly(ggplot(data = highlight_key(fc_sample_pivot(), ~gene), 
+                                        aes(x=fct_reorder(gene, val), y=val, fill=fold_cat, text=condition)) +
+                                   geom_col(position="stack",alpha=1,color='black',linewidth=0.2) +
+                                   geom_hline(yintercept=0, size=1) +
+                                   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank(),
+                                         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+                                   scale_fill_manual(values=fold_cat_colors, name="Fold Change",
+                                                     breaks=c("-2", "-1.5", "-0.75", "-0.25", "0.25", "0.75", "1.5", "2")), tooltip = "text"),
+                        on = "plotly_hover", opacityDim=0.3)
+      } else {
+        highlight(ggplotly(ggplot(data = highlight_key(fc_sample_pivot(), ~condition), 
+                                  aes(x=fct_reorder(gene, val), y=val, fill=fold_cat, text=condition)) +
+                             geom_col(position="stack",alpha=1,color='black',linewidth=0.2) +
+                             geom_hline(yintercept=0, size=1) +
+                             theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank(),
+                                   axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+                             scale_fill_manual(values=fold_cat_colors, name="Fold Change",
+                                               breaks=c("-2", "-1.5", "-0.75", "-0.25", "0.25", "0.75", "1.5", "2")), tooltip = "text"),
+                  on = "plotly_hover", opacityDim=0.3)
+      }
     }) 
 }
--- TileMap/ui.R
+++ TileMap/ui.R
@@ -2,17 +2,20 @@ library(shiny)
 library(plotly)
 
 # Define UI for application that draws a histogram
-choices = unique(fc_sample_pivot$condition)
+datasets = fs::path_ext_remove(unique(list.files("datasets/")))
+print(datasets)
 
 fluidPage(
 
     # Application title
-    titlePanel("TrendMap"),
+    titlePanel("TileMap"),
 
     # Sidebar with a slider input for number of bins
     verticalLayout(
         
         # Show a plot of the generated distribution
+        selectInput("dataset", "Choose Dataset", choices=datasets, selected=datasets[0]),
+        
         mainPanel(
             plotlyOutput("tilePlot"),
             width=10
